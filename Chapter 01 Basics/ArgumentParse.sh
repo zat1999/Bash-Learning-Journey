3642a0e4371480382e030a6d11b7959432888d84
Zats <111195788+zat1999@users.noreply.github.com>
2024-06-22 22:02:38 +0800

    Delete Chapter 01 Basics directory

--- Chapter 01 Basics/ArgumentParse.sh
+++
@@ -1,4 +0,0 @@
-#!/user/bin/env bash
-#note needs to be double quotation (strong quoating)
-printf "hello, %s\n" "$1"
-
--- Chapter 01 Basics/UserInput.sh
+++
@@ -1,6 +0,0 @@
-#!/user/bin/env bash
-echo "Who are you?"
-read name
-echo "Hello, $name"
-echo "Strong Quote: $name"
-echo 'Weak Quote: $name' 
